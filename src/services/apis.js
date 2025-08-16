d5f735e29f28d9bba76bd4048e556459dcf440e7
Ameen Sid <ameensid7@outlook.com>
2025-08-16 21:12:01 +0530

    Fix some change and update backend service url

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 // const BASE_URL = process.env.REACT_APP_BASE_URL;
-const BASE_URL = "http://localhost:4000/api/v1";
+const BASE_URL = "https://codehell-backend.onrender.com/api/v1";
 
 // Auth Endpoints
 export const endpoints = {
